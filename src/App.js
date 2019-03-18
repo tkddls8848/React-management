37d9944166ed5454e896c8ef144698543fb57551
tkddls8848 <tkddls8848@gmail.com>
2019-03-18 13:38:40 +0900

    구조화 된 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer={
+  name:'홍길동',
+  birthday:'123456',
+  gender:'남자',
+  job:'무직'
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="gray-background">
-        <img src={logo} lat='logo'></img>
-        <p2>START</p2>
-      </div>
+    return(
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      ></Customer>
     );
   }
 }

c27e71cc07efa2fff872e329a3c2b9ced497a9d2
tkddls8848 <tkddls8848@gmail.com>
2019-04-04 18:37:13 +0900

    고객 관리 시스템 네비게이션 바, 검색기능

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,105 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
-const styles = theme => ({
+const styles = (theme) => ({
   root:{
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto'
+    width : '100%',
+    minWidth : 1080,
+    marginTop : theme.spacing.unit * 3,
+    overflowX : 'auto'
   },
-  table:{
-    minWidth: 1080
+  menu:{
+    marginTop : 15,
+    marginBottom : 15,
+    display : 'flex',
+    justifyContent : 'center'
+  },
+  tablehead:{
+    fontSize:'1.3rem'
+  },
+  paper:{
+    marginLeft:20,
+    marginRight:20
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
   },
   progress: {
     margin: theme.spacing.unit *2
@@ -25,19 +115,26 @@ const styles = theme => ({
 
 class App extends Component {
 
-
   constructor(props){
     super(props);
     this.state = {
       customers:"",
-      completed: 0
+      completed: 0,
+      searchKeyword : ""
     }
+  }
+
+  handleValueChange = (e) => {
+    let nextState = {}
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
   }
 
   stateRefresh = () => {
     this.setState({
-      customers:"",
-      completed: 0
+      customers : '',
+      completed : 0,
+      searchKeyword : ''
     });
     this.callApi()
     .then(body => this.setState({customers:body}))
@@ -64,32 +161,65 @@ class App extends Component {
 
   render() {
     const {classes} = this.props;
+    const cellList = ['번호', '이미지','이름','생년월일','성별','직업','삭제'];
+    const filteredComponents = (data) => {
+      data = data.filter((keyword) => {
+        return keyword.name.indexOf(this.state.searchKeyword) > -1;
+      });
+      return data.map((component) => {
+        return(
+          <Customer
+          stateRefresh={this.stateRefresh}
+          id={component.id}
+          key={component.id}
+          image={component.image}
+          name={component.name}
+          birthday={component.birthday}
+          gender={component.gender}
+          job={component.job}/> 
+        );
+      })
+    }
     return(
-      <div>
-        <Paper className={classes.root}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              고객 관리 시스템
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="검색하기"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                name="searchKeyword"
+                value={this.state.searchKeyword}
+                onChange={this.handleValueChange}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.menu}>
+          <CustomerAdd stateRefresh={this.stateRefresh}></CustomerAdd>
+        </div>
+      <Paper className={classes.paper}>
           <Table className={classes.table}>
               <TableRow className={classes.table}>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-                <TableCell>삭제</TableCell>
+                {cellList.map((head)=>{
+                  return <TableCell className={classes.tablehead}>{head}</TableCell>
+                })}
               </TableRow>
-              {this.state.customers ? this.state.customers.map((object) => {
-                return(
-                  <Customer
-                  stateRefresh={this.stateRefresh}
-                  id={object.id}
-                  key={object.id}
-                  image={object.image}
-                  name={object.name}
-                  birthday={object.birthday}
-                  gender={object.gender}
-                  job={object.job}/> 
-                );
-              })
+              {this.state.customers ? 
+                filteredComponents(this.state.customers)
               : 
               <TableRow>
                 <TableCell colSpan="6" align="center">
@@ -99,9 +229,7 @@ class App extends Component {
               }
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh}></CustomerAdd>
-      </div>
-
+    </div>
     )
   };
 };
--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CustomerDelete from './CustomerDelete';
 
-class Customer extends Component{
-    render(){
-        return(
+class Customer extends Component {
+    render() {
+        return (
             <TableRow>
-                <TableCell component="th">{this.props.id}</TableCell>
-                <TableCell><img src={this.props.image} alt='profile'></img></TableCell>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
                 <TableCell>{this.props.job}</TableCell>
-                <TableCell><CustomerDelete id={this.props.id} stateRefresh={this.props.stateRefresh}></CustomerDelete></TableCell>
+                <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>
             </TableRow>
         )
     }
